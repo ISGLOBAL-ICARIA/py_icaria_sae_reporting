5db8d1fe086224230ee7a9f8f54694ddc1d5ffee
Andreu Bofill <andreu.bp@gmail.com>
2022-11-25 16:48:49 +0100

    Change all the function to incorporate the I&F option

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ __credits__ = ["Maximo Ramirez Robles"]
 __license__ = "MIT"
 __version__ = "0.0.1"
 __date__ = "20210629"
-__maintainer__ = "Maximo Ramirez Robles"
-__email__ = "maximo.ramirez@isglobal.org"
+__maintainer__ = "Andreu Bofill"
+__email__ = "andreu.bofill@isglobal.org"
 __status__ = "Dev"
 
 if __name__ == '__main__':
@@ -45,19 +45,21 @@ if __name__ == '__main__':
         # Merge SAE data with Study Numbers to produce SAE Numbers
         df = df_sae.merge(df_sn, on='record_id')
         to_import = []
+        previous_sae_number = ''
+        previous_index = ''
         for index, row in df.iterrows():
-            if pandas.isna(row['sae_number']):
-                sae_number = row['study_number']
-                # First SAE report on the participant. SAE Number = Study Number-01
-                if df.index.value_counts()[index] == 1:
-                    sae_number = sae_number + "-01"
-                # For the consecutive SAE reports, the SAE number will be the Study Number concatenated with the number
-                # of initial reports
-                # TODO: This is assuming that we are numbering just one initial report by script execution. If there are
-                #       more than one initial reports to number. As it is, the script will use the same number for all
-                #       the reports.
+            if index != previous_index:
+                previous_sae_number = ''
+            if pandas.isna(row['sae_number']) or "-" not in row['sae_number'].split("ICA-")[1]:
+                if previous_sae_number=='':
+                    sae_number = row['study_number'] + "-01"
                 else:
-                    sae_number = sae_number + "-0" + str(df.loc[index].sae_report_type.value_counts()[1])
+                    if row['sae_report_type'] in [1,4]:
+                        next_number = int(previous_sae_number.split("-")[2])+1
+
+                    elif row['sae_report_type'] in [2,3]:
+                        next_number = int(previous_sae_number.split("-")[2])
+                    sae_number = str(row['study_number']) + "-0" + str(next_number)
 
                 record_dict = {
                     'record_id': index,
@@ -66,7 +68,14 @@ if __name__ == '__main__':
                     'redcap_repeat_instance': row['redcap_repeat_instance_x'],
                     'sae_number': sae_number
                 }
+                print (index)
                 to_import.append(record_dict)
+#                print(sae_number)
+#                print(record_dict)
+                previous_sae_number = sae_number
+            else:
+                previous_sae_number = row['sae_number']
+            previous_index=index
 
         # Import new created SAE Numbers
         print("[{}] Importing SAE Numbers into {}...".format(datetime.now(), project_key))
